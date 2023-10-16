ab386f7372a2a3dbac05906b7d998b7c6077d356
Pedro Rambo <phhrambo@gmail.com>
2023-10-15 22:02:19 -0300

    Improved relay server messages

--- relay-server/RelayServer.py
+++ relay-server/RelayServer.py
@@ -10,6 +10,7 @@ DEFAULT_PORT = 5000
 def udpServer():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.bind(('0.0.0.0', DEFAULT_PORT))
+    print("Server listening on UDP port " + str(DEFAULT_PORT))
 
     while True:
         data, client = s.recvfrom(BUFFER_SIZE)
@@ -20,18 +21,18 @@ def udpServer():
                 username = message.split(" ")[1]
                 Registration.update_last_response(
                     username, client[0], client[1])
-                print("keepalive " + username)
+                print("[KEEPALIVE] " + username)
             if (message.startswith("REGISTER")):
                 message = message.replace("\n", "")
                 username = message.split(" ")[1]
                 Registration.add(username, client[0], client[1], s)
-                print("REGISTER " + username)
+                print("[REGISTER] " + username)
             if (message.startswith("SEND")):
                 message = message.replace("\n", "")
                 username = message.split(" ")[1]
                 content = " ".join(message.split(" ")[2:])
                 Registration.send_message(username, content)
-                print("SEND " + username)
+                print("[SEND] " + username)
 
 
 def send_keepalives():
@@ -42,5 +43,4 @@ def send_keepalives():
 
 t = threading.Thread(target=send_keepalives)
 t.start()
-
 udpServer()
