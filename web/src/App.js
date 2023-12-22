f87a0d72a27d27de22cbceedbeb1497d8fac8f31
Pedro Rambo <phhrambo@gmail.com>
2023-12-22 13:31:18 -0300

    Added list friends from Api to web

--- web/src/App.js
+++ web/src/App.js
@@ -1,19 +1,50 @@
 import "./index.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [friends, setFriends] = useState([]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      fetch("http://localhost:5000/friends")
+        .then((res) => res.json())
+        .then((data) => {
+          setFriends(
+            data.map((f) => {
+              if(!f.lastHeartbeat) return f
+              const lastHeartbeat = new Date(f.lastHeartbeat);
+              const now = new Date();
+              const diff = now - lastHeartbeat;
+              const differenceInSeconds = Math.floor(diff / 1000)
+              return {
+                ...f,
+                differenceInSeconds,
+                active: diff < 15000
+              };
+            })
+          );
+        });
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className="main-container">
       <div className="friends-container">
         <div className="friends-list">
-          <a href="#" className="friend">
-            <div href="#" className="friend-horizontal">
-              <div className="friend-status"></div>
-              <div>
-                <h3>username</h3>
-                <p className="last-seen">3 seconds</p>
+          {friends.map((f) => (
+            <a href="#" className="friend" key={f.username}>
+              <div href="#" className="friend-horizontal">
+                <div className={`friend-status ${f.active ? "online" : ""}`}></div>
+                <div>
+                  <h3>{f.username}</h3>
+                  {typeof f.differenceInSeconds === 'number' && (
+                    <p className="last-seen">{f.differenceInSeconds} {f.differenceInSeconds > 1 ? "seconds" : "second"}</p> 
+                  )}
+                </div>
               </div>
-            </div>
-          </a>
+            </a>
+          ))}
         </div>
 
         <a href="#" type="button" className="btn">
