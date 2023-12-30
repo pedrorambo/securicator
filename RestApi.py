fff08fb4ad51666c3c2b24273cc64e8b1f194ec0
Pedro Rambo <phhrambo@gmail.com>
2023-12-30 09:05:33 -0300

    Added general exception handler to RestApi

--- RestApi.py
+++ RestApi.py
@@ -19,46 +19,52 @@ def sanitize_filename(input):
 
 class BaseServer(BaseHTTPRequestHandler):
     def general_handler(self, method):
-        parts = self.path.split("?")
-        parsed_url = urlparse(self.path)
-        query = parse_qs(parsed_url.query)
-        path = parts[0]
-        found = False 
+        try:
+            parts = self.path.split("?")
+            parsed_url = urlparse(self.path)
+            query = parse_qs(parsed_url.query)
+            path = parts[0]
+            found = False 
 
-        body = None
+            body = None
 
-        content_len_raw = self.headers.get('Content-Length')
-        if content_len_raw != None:
-            post_body = self.rfile.read(int(content_len_raw))
-            body = json.loads(post_body)
+            content_len_raw = self.headers.get('Content-Length')
+            if content_len_raw != None:
+                post_body = self.rfile.read(int(content_len_raw))
+                body = json.loads(post_body)
 
-        for handler in RestApi.handlers:
-            if handler.method == method and path.startswith(handler.path):
-                found = True
-                if handler.function != None:
-                    response = handler.function(query=query, body=body)
-                    if response == None:
-                        self.send_response(201)
-                        self.send_header("Access-Control-Allow-Origin", "*")
-                        self.end_headers()
+            for handler in RestApi.handlers:
+                if handler.method == method and path.startswith(handler.path):
+                    found = True
+                    if handler.function != None:
+                        response = handler.function(query=query, body=body)
+                        if response == None:
+                            self.send_response(201)
+                            self.send_header("Access-Control-Allow-Origin", "*")
+                            self.end_headers()
+                        else:
+                            self.send_response(200)
+                            self.send_header("Content-type", "application/json")
+                            self.send_header("Access-Control-Allow-Origin", "*")
+                            self.end_headers()
+                            self.wfile.write(bytes(json.dumps(response), "utf-8"))
                     else:
-                        self.send_response(200)
-                        self.send_header("Content-type", "application/json")
-                        self.send_header("Access-Control-Allow-Origin", "*")
-                        self.end_headers()
-                        self.wfile.write(bytes(json.dumps(response), "utf-8"))
-                else:
-                    end_path = os.path.basename(path)
-                    with open(handler.base_dir + sanitize_filename(file_name(end_path)) + "." + sanitize_filename(file_extension(end_path)), "rb") as f:
-                        self.send_response(200)
-                        self.send_header("Access-Control-Allow-Origin", "*")
-                        self.end_headers()
-                        self.wfile.write(f.read())
+                        end_path = os.path.basename(path)
+                        with open(handler.base_dir + sanitize_filename(file_name(end_path)) + "." + sanitize_filename(file_extension(end_path)), "rb") as f:
+                            self.send_response(200)
+                            self.send_header("Access-Control-Allow-Origin", "*")
+                            self.end_headers()
+                            self.wfile.write(f.read())
+        
 
 
-        if not found:
-            self.send_response(404)
-            self.end_headers()
+            if not found:
+                self.send_response(404)
+                self.end_headers()
+        except Exception as e:
+            print(str(e))
+            pass
+            
             
     def log_message(a, b, c, d, e):
         pass
