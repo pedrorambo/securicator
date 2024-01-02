7a2e924ed3246b71a184a3c527142b00e26d2bcf
Pedro Rambo <phhrambo@gmail.com>
2024-01-02 20:53:37 -0300

    Refactored connection persistence, and stability with the relay server

--- Relay.py
+++ Relay.py
@@ -1,5 +1,3 @@
-import socket
-import threading
 import time
 from App import App
 
@@ -14,37 +12,21 @@ class Relay:
         Relay.server_ip = server
         Relay.server_port = port
         Relay.segment = Segment(App.get_username(), Relay.receive, Relay.server_ip, Relay.server_port)
-        Relay.connected = True
         
     def receive(data):
-        Relay.connected = True
         if data:
-            Relay.handle_message(data.decode())
+            decoded = None
+            try:
+                decoded = data.decode()
+            except Exception as e:
+                print("Failed to decode received message: ", str(e))
+            if decoded != None:
+                Relay.handle_message(data.decode())
 
     @staticmethod
     def setup(username, handle_message):
         Relay.handle_message = handle_message
-                    
-        def healthcheck():
-            time.sleep(10)
-            while True:
-                time.sleep(5)
-                if Relay.segment.get_last_received() is not None and int(current_time_in_milliseconds()) - int(Relay.segment.get_last_received()) > 15000:
-                    print("Realy server timed out, registering again...")
-                    Relay.connected = False
-                    try:
-                        Relay.segment.close()
-                        Relay.segment = Segment(App.get_username(), Relay.receive, Relay.server_ip, Relay.server_port)
-                        Relay.connected = True
-                    except Exception as e:
-                        print(str(e))
-        
-
-        keepalive_thread = threading.Thread(target=healthcheck)
-        keepalive_thread.start()
 
     @staticmethod
     def send(username, content):
         Relay.segment.send(username, content.encode("utf-8"))
-
-Relay.connected = False
--- Segment.py
+++ Segment.py
@@ -1,60 +1,97 @@
-from queue import PriorityQueue, SimpleQueue
-import hashlib
+import logging
+from queue import PriorityQueue
 import socket
 import threading
 import time
 
 
 def current_time_in_milliseconds():
-    return str(round(time.time() * 1000))
+    return round(time.time() * 1000)
 
+CONNECTION_EXPIRE_TIMEOUT_IN_MILLISECONDS = 15000
 MAX_SEGMENT_SIZE_IN_BYTES = 500000
 
 class Segment:
     def __init__(self, username, callback, server_ip, server_port):
-        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.s.connect((server_ip, server_port))
-        null_padded = username.encode('utf-8') +  (b' ' * (500 - len(username.encode("utf-8"))))
-        self.s.sendall(null_padded)
-        self.last_received = 1
-        
+        self.server_ip = server_ip
+        self.server_port = server_port
+        self.username = username
+        self.s = None
+        self.connected = False
+        self.last_received = None
+        self.connection_keeping_thread = threading.Thread(target=self.keep_connection)
+        self.connection_keeping_thread.start()
         self.receive_thread = threading.Thread(target=self.receive)
         self.receive_thread.start()
         self.callback = callback
-        
         self.keepalive_thread = threading.Thread(target=self.keepalive)
         self.keepalive_thread.start()
-        
-        self.sending = False
         self.queue = PriorityQueue()
-        
         self.sender_thread = threading.Thread(target=self.sender)
         self.sender_thread.start()
         
+        logging.info("Relay server connected")
+        
     def keepalive(self):
-        try:
-            while True:
-                time.sleep(5)
-                self.s.sendall(b'\x00\x00\x00\x00')
-        except:
-            print("Error in send keepalive")
-            pass
+        while True:
+            try:
+                while self.connected == False:
+                    print("Sleeping not connected")
+                    time.sleep(1)
+                if self.connected:
+                    self.s.sendall(b'\x00\x00\x00\x00')
+            except:
+                print("Keepalive failed, setting not connected")
+                self.connected = False
+                print("Error in send keepalive")
+                pass
+            time.sleep(5)
+        
+    def connect(self):
+        print("Connecting...")
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.s.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
+        self.s.settimeout(5)
+        self.s.connect((self.server_ip, self.server_port))
+        # self.s.settimeout(None)
+        self.connected = True
+        null_padded = self.username.encode('utf-8') +  (b' ' * (500 - len(self.username.encode("utf-8"))))
+        self.s.sendall(null_padded)
+        self.last_received = None
+        print("Connected")
+
+    def keep_connection(self):
+        while True:
+            expired = self.last_received != None and current_time_in_milliseconds() - self.last_received >= CONNECTION_EXPIRE_TIMEOUT_IN_MILLISECONDS
+            if (not self.connected) or expired:
+                try:
+                    if self.s != None:
+                        self.close()
+                    self.connect()
+                except Exception as e:
+                    print(str(e))
+                    pass
+            time.sleep(1)
         
     def sender(self):
-        try:
             while True:
-                priority, username, content = self.queue.get()
-                magic = b'\x00\x00\x00\x01'
-                if len(content) > MAX_SEGMENT_SIZE_IN_BYTES:
-                    raise Exception("Segment too large")
-                null_padded = username.encode("utf-8") +  (b' ' * (500 - len(username.encode("utf-8"))))
-                inner_content = null_padded + content
-                length = len(inner_content)
-                b  = magic + length.to_bytes(4, byteorder="big") + inner_content
-                self.s.sendall(b)
-        except:
-            print("Error in send packet")
-            pass
+                try:
+                    while not self.connected:
+                        print("Not connected")
+                        time.sleep(1)
+                    priority, username, content = self.queue.get()
+                    magic = b'\x00\x00\x00\x01'
+                    if len(content) > MAX_SEGMENT_SIZE_IN_BYTES:
+                        raise Exception("Segment too large")
+                    null_padded = username.encode("utf-8") +  (b' ' * (500 - len(username.encode("utf-8"))))
+                    inner_content = null_padded + content
+                    length = len(inner_content)
+                    b  = magic + length.to_bytes(4, byteorder="big") + inner_content
+                    self.s.sendall(b)
+                except:
+                    self.connected = False
+                    print("Error in send packet")
+                    pass
         
     def send(self, username, content):
         priority = 1
@@ -63,68 +100,89 @@ class Segment:
         self.queue.put((priority, username, content))
         
     def get_last_received(self):
-        return self.last_received
+        if self.last_received == None:
+            return 1
+        else:
+            return self.last_received
         
     def receive(self):
-        try:
             length = None
             total = b''
             inner_content = b''
             buffer = b''
             while True:
-                content = self.s.recv(1024)
-                buffer += content
-                self.last_received = current_time_in_milliseconds()
-                
-                # MAGIC NUMBER - 4 bytes
-                # LENGTH       - 4 bytes
-                # CONTENT 
-                
-                if len(content) == 0:
-                    break
-                
-                is_expecting_a_new_packet = length == None
-                if is_expecting_a_new_packet:
-                    if len(buffer) < 4:
-                        print("Not enough data in packet")
-                        continue
-                    magic = buffer[0:4]
-                    if magic == b'\x00\x00\x00\x00':
-                        buffer = buffer[4:]
-                        #print("Received keepalive")
-                        continue
-                    if magic != b'\x00\x00\x00\x01':
-                        buffer = buffer[4:]
-                        print("Incorrect magic number")
-                        continue
-                    length = int.from_bytes(buffer[4:8], byteorder="big")
-                    if length > MAX_SEGMENT_SIZE_IN_BYTES:
-                        print("Received segment too large")
-                        buffer = buffer[4:]
-                        continue
-                    total = buffer[0:8]
-                    buffer = buffer[8:]
-                
-                if len(buffer) < length:
-                    continue
-                else:
-                    inner_content = buffer[0:length]
-                    total = total + inner_content
-                    buffer = buffer[length:]
+                try:
+                    while not self.connected:
+                        print("Waiting for connected socket to listen")
+                        time.sleep(1)
+                    content = self.s.recv(1024)
+                    buffer += content
+                    self.last_received = current_time_in_milliseconds()
                     
-                if length != None and len(inner_content) >= length:
-                    self.callback(inner_content[500:])
-                    length = None
-                    total = b''
-                    inner_content = b''
-        except Exception as e:
-            print("Error: ", str(e))
-            pass
+                    # MAGIC NUMBER - 4 bytes
+                    # LENGTH       - 4 bytes
+                    # CONTENT 
+                    
+                    if len(content) == 0:
+                        self.connected = False
+                        continue
+                    
+                    is_expecting_a_new_packet = length == None
+                    if is_expecting_a_new_packet:
+                        if len(buffer) < 4:
+                            print("Not enough data in packet")
+                            continue
+                        should_continue = False
+                        while len(buffer) >= 4:
+                            magic = buffer[0:4]
+                            if magic == b'\x00\x00\x00\x00':
+                                buffer = buffer[4:]
+                                #print("Received keepalive")
+                                continue
+                            if magic != b'\x00\x00\x00\x01':
+                                buffer = buffer[4:]
+                                print("Incorrect magic number")
+                                continue
+                            else:
+                                should_continue = True
+                                break
+                        if not should_continue:
+                            continue
+                        length = int.from_bytes(buffer[4:8], byteorder="big")
+                        if length > MAX_SEGMENT_SIZE_IN_BYTES:
+                            print("Received segment too large")
+                            buffer = buffer[4:]
+                            continue
+                        total = buffer[0:8]
+                        buffer = buffer[8:]
+                    
+                    if len(buffer) < length:
+                        continue
+                    else:
+                        inner_content = buffer[0:length]
+                        total = total + inner_content
+                        buffer = buffer[length:]
+                        
+                    if length != None and len(inner_content) >= length:
+                        self.callback(inner_content[500:])
+                        length = None
+                        total = b''
+                        inner_content = b''
+                except socket.timeout:
+                    pass
+                except Exception as e:
+                    if "reset by peer" in str(e):
+                        pass
+                    else:
+                        raise e
+                    # print("Error: ", str(e))
+                    # pass
                 
     def close(self):
         print("Closing socket")
         try:
             self.s.close()
+            self.connected = False
         except Exception as e:
             print(str(e))
             pass
