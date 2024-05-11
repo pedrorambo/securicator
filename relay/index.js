0882350b4b6a71d7af373065fa1bca32fd7e58df
Pedro Rambo <pedro.rambo@fortesecurity.com.br>
2024-05-11 08:25:40 -0300

    Added relay module

---
+++ relay/index.js
@@ -0,0 +1,27 @@
+import WebSocket, { WebSocketServer } from "ws";
+
+const wss = new WebSocketServer({ port: 9090 });
+
+wss.on("connection", function connection(ws) {
+  ws.on("error", console.error);
+
+  ws.on("message", function message(data, isBinary) {
+    const textContent = data.toString();
+    if (textContent.startsWith("CONNECT ")) {
+      const publicKey = textContent.split(" ")[1];
+      ws.publicKey = publicKey;
+      return;
+    }
+
+    const destinationPublicKey = textContent.split(" ")[1];
+
+    wss.clients.forEach(function each(client) {
+      if (client === ws) return;
+      if (client.publicKey !== destinationPublicKey) return;
+      console.log(`Message ${data.length}`);
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(data, { binary: isBinary });
+      }
+    });
+  });
+});
