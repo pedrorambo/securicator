fa702408b51d69049207d98d14bd74ae24f9e189
Pedro Rambo <phhrambo@gmail.com>
2024-07-08 14:10:33 -0300

    Added automatic reload on new version

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -5,8 +5,35 @@ import { Home } from "./pages/Home";
 import { AddContact } from "./pages/AddContact";
 import { Chat } from "./pages/Chat";
 import { ChangeName } from "./pages/ChangeName";
+import { useEffect } from "react";
+
+const COMPILED_COMMIT_ID = process.env.CF_PAGES_COMMIT_SHA;
 
 function App() {
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const url = `${window.location.origin}/version.txt`;
+      fetch(url, {
+        cache: "no-cache",
+      }).then((response) => {
+        response.text().then((response) => {
+          const latestCommitId = response;
+          if (
+            latestCommitId.length === 40 &&
+            COMPILED_COMMIT_ID?.length === 40
+          ) {
+            if (latestCommitId !== COMPILED_COMMIT_ID) {
+              window.location.reload();
+            }
+          }
+        });
+      });
+    }, 10000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
   const router = createBrowserRouter([
     {
       path: "/",
