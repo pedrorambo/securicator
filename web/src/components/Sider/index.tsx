fbdf6f78097a56acf9973aee064af6bb886b9332
Pedro Rambo <phhrambo@gmail.com>
2024-07-24 13:27:58 -0300

    Improved top bar button color

--- web/src/components/Sider/index.tsx
+++ web/src/components/Sider/index.tsx
@@ -55,7 +55,7 @@ export function Sider() {
             <svg
               stroke="currentColor"
               fill="currentColor"
-              stroke-width="0"
+              strokeWidth="0"
               viewBox="0 0 512 512"
               height="200px"
               width="200px"
--- web/src/pages/TopMenu/index.tsx
+++ web/src/pages/TopMenu/index.tsx
@@ -23,7 +23,7 @@ export const TopMenu: FC<Props> = ({ hideBackButton, subtitle, title }) => {
           <svg
             stroke="currentColor"
             fill="currentColor"
-            stroke-width="0"
+            strokeWidth="0"
             viewBox="0 0 320 512"
             height="200px"
             width="200px"
