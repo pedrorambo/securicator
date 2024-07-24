65f1167295d11754de6c08b73f5509e88c514d28
Pedro Rambo <phhrambo@gmail.com>
2024-07-24 17:56:17 -0300

    Added scroll to main page

--- web/src/pages/Chats/index.tsx
+++ web/src/pages/Chats/index.tsx
@@ -52,143 +52,153 @@ export const Chats: FC<Props> = () => {
     <>
       <main>
         <TopMenu hideBackButton title="Contacts" />
-        <ul className="contacts" id="friends">
-          {!contacts?.length && (
-            <p className="text-muted">You have no contacts yet.</p>
-          )}
-          {contacts.map((c) => (
-            <li
-              key={c.publicKey}
-              className={c.publicKey === publicKey ? "open" : ""}
-            >
-              <Link to={`/contacts/${encodeURIComponent(c.publicKey)}`}>
-                <h3 className={c.unread ? "unread" : ""}>
-                  {c.lastSeenAt &&
-                  now.getTime() - c.lastSeenAt.getTime() < 1000 * 5 ? (
-                    <div className="connected"></div>
-                  ) : (
-                    <div className="disconnected"></div>
-                  )}{" "}
-                  {truncateText(c.displayName || c.publicKey)}
-                </h3>
-                <span>{truncateText(c.biography)}</span>
-              </Link>
-            </li>
-          ))}
-        </ul>
-
-        <hr />
-
-        <h3>Options</h3>
-
-        <Link to="/contacts/new" className="btn btn-text">
-          Add new contact
-        </Link>
-        <Link to="/name" className="btn btn-text">
-          Edit profile
-        </Link>
-        <button
-          className="btn btn-text"
-          onClick={() => {
-            navigator.clipboard
-              .writeText(globalPublicKey || "")
-              .then(() => {
-                setCopied(true);
-              })
-              .catch(console.error);
-            if (navigator.share) {
-              navigator
-                .share({
-                  text: globalPublicKey || "",
-                })
-                .catch(console.error);
-            }
+        <div
+          style={{
+            overflowY: "auto",
+            display: "flex",
+            flexDirection: "column",
+            gap: ".25rem",
           }}
         >
-          {copied ? "Copied" : "Copy public key"}
-        </button>
+          <ul className="contacts" id="friends">
+            {!contacts?.length && (
+              <p className="text-muted">You have no contacts yet.</p>
+            )}
 
-        <hr />
+            {contacts.map((c) => (
+              <li
+                key={c.publicKey}
+                className={c.publicKey === publicKey ? "open" : ""}
+              >
+                <Link to={`/contacts/${encodeURIComponent(c.publicKey)}`}>
+                  <h3 className={c.unread ? "unread" : ""}>
+                    {c.lastSeenAt &&
+                    now.getTime() - c.lastSeenAt.getTime() < 1000 * 5 ? (
+                      <div className="connected"></div>
+                    ) : (
+                      <div className="disconnected"></div>
+                    )}{" "}
+                    {truncateText(c.displayName || c.publicKey)}
+                  </h3>
+                  <span>{truncateText(c.biography)}</span>
+                </Link>
+              </li>
+            ))}
+          </ul>
 
-        <h3>Sensitive actions</h3>
+          <hr />
 
-        <button
-          className="btn btn-text btn-text-danger"
-          onClick={() => {
-            const response = window.confirm(
-              "Are you sure you want to copy or share the synchronization key? Any person with the key can access your account."
-            );
-            if (response) {
+          <h3>Options</h3>
+
+          <Link to="/contacts/new" className="btn btn-text">
+            Add new contact
+          </Link>
+          <Link to="/name" className="btn btn-text">
+            Edit profile
+          </Link>
+          <button
+            className="btn btn-text"
+            onClick={() => {
               navigator.clipboard
-                .writeText(synchronizationKey || "")
+                .writeText(globalPublicKey || "")
                 .then(() => {
-                  setCopiedSynchronization(true);
+                  setCopied(true);
                 })
                 .catch(console.error);
               if (navigator.share) {
                 navigator
                   .share({
-                    text: synchronizationKey || "",
+                    text: globalPublicKey || "",
                   })
                   .catch(console.error);
               }
-            }
-          }}
-        >
-          {copiedSynchronization ? "Copied" : "Copy synchronization key"}
-        </button>
+            }}
+          >
+            {copied ? "Copied" : "Copy public key"}
+          </button>
 
-        <button
-          className="btn btn-text btn-text-danger"
-          onClick={() => {
-            const response = window.confirm(
-              "Are you sure you want to delete everything?"
-            );
-            if (response) {
-              const response2 = window.confirm(
-                "Are you really sure you want to delete everything?"
+          <hr />
+
+          <h3>Sensitive actions</h3>
+
+          <button
+            className="btn btn-text btn-text-danger"
+            onClick={() => {
+              const response = window.confirm(
+                "Are you sure you want to copy or share the synchronization key? Any person with the key can access your account."
               );
-              if (response2) {
-                window.localStorage.clear();
-                window.indexedDB
-                  .databases()
-                  .then((r) => {
-                    for (var i = 0; i < r.length; i++)
-                      // @ts-ignore
-                      window.indexedDB.deleteDatabase(r[i].name);
-                  })
+              if (response) {
+                navigator.clipboard
+                  .writeText(synchronizationKey || "")
                   .then(() => {
-                    window.location.replace("/");
-                  });
+                    setCopiedSynchronization(true);
+                  })
+                  .catch(console.error);
+                if (navigator.share) {
+                  navigator
+                    .share({
+                      text: synchronizationKey || "",
+                    })
+                    .catch(console.error);
+                }
               }
-            }
-          }}
-        >
-          Delete everything
-        </button>
+            }}
+          >
+            {copiedSynchronization ? "Copied" : "Copy synchronization key"}
+          </button>
 
-        <hr />
+          <button
+            className="btn btn-text btn-text-danger"
+            onClick={() => {
+              const response = window.confirm(
+                "Are you sure you want to delete everything?"
+              );
+              if (response) {
+                const response2 = window.confirm(
+                  "Are you really sure you want to delete everything?"
+                );
+                if (response2) {
+                  window.localStorage.clear();
+                  window.indexedDB
+                    .databases()
+                    .then((r) => {
+                      for (var i = 0; i < r.length; i++)
+                        // @ts-ignore
+                        window.indexedDB.deleteDatabase(r[i].name);
+                    })
+                    .then(() => {
+                      window.location.replace("/");
+                    });
+                }
+              }
+            }}
+          >
+            Delete everything
+          </button>
 
-        <h3>Debug tools</h3>
+          <hr />
 
-        <Link to="/envelopes" className="btn btn-text">
-          Envelopes
-        </Link>
+          <h3>Debug tools</h3>
 
-        <Link to="/events" className="btn btn-text">
-          Events
-        </Link>
+          <Link to="/envelopes" className="btn btn-text">
+            Envelopes
+          </Link>
 
-        <Link to="/contacts" className="btn btn-text">
-          Contacts
-        </Link>
+          <Link to="/events" className="btn btn-text">
+            Events
+          </Link>
 
-        <span className="text-muted version" title={COMPILED_COMMIT_ID}>
-          Version:{" "}
-          <pre style={{ display: "inline" }}>
-            {COMPILED_COMMIT_ID.substring(0, 8)}
-          </pre>
-        </span>
+          <Link to="/contacts" className="btn btn-text">
+            Contacts
+          </Link>
+
+          <span className="text-muted version" title={COMPILED_COMMIT_ID}>
+            Version:{" "}
+            <pre style={{ display: "inline" }}>
+              {COMPILED_COMMIT_ID.substring(0, 8)}
+            </pre>
+          </span>
+        </div>
       </main>
     </>
   );
