adc06151609e8d47bc3c6d200dbdfe65d1444b8d
Pedro Rambo <phhrambo@gmail.com>
2024-07-24 15:51:04 -0300

    Added mutex to send message

--- relay/index.js
+++ relay/index.js
@@ -10,55 +10,69 @@ const wss = new WebSocketServer({
   maxPayload: 1024 * 1024,
 });
 
+// FIXME: Messages were not being delivered in order. Need to add a mutex.
+let handlingMessage = false;
+
 wss.on("connection", function connection(ws) {
   ws.on("error", console.error);
 
-  ws.on("message", function message(data, isBinary) {
-    const textContent = data.toString();
-    if (textContent.startsWith("CONNECT ")) {
-      const publicKey = textContent.split(" ")[1];
-      ws.publicKey = publicKey;
+  ws.on("message", async function message(data, isBinary) {
+    while (handlingMessage) {
+      await new Promise((resolve) => setTimeout(resolve, 1));
+    }
+    try {
+      handlingMessage = true;
+      const textContent = data.toString();
+      if (textContent.startsWith("CONNECT ")) {
+        const publicKey = textContent.split(" ")[1];
+        ws.publicKey = publicKey;
 
-      const queuedItems = queue.filter((item) => item.to === publicKey);
-      queue = queuedItems.filter((item) => item.to !== publicKey);
-      console.log("MEESSAGES REMOVED FROM QUEUE ", queue.length);
+        const queuedItems = queue.filter((item) => item.to === publicKey);
+        queue = queuedItems.filter((item) => item.to !== publicKey);
+        console.log("MEESSAGES REMOVED FROM QUEUE ", queue.length);
 
-      for (const item of queuedItems) {
-        ws.send(item.data, { binary: data.isBinary });
+        for (const item of queuedItems) {
+          ws.send(item.data, { binary: data.isBinary });
+        }
+
+        return;
       }
 
-      return;
-    }
-
-    if (textContent.startsWith("PING")) {
-      ws.send("PONG");
-      return;
-    }
-
-    const destinationPublicKey = textContent.split(" ")[1];
-    const retentionLevel = textContent.split(" ")[2];
-
-    let destionationWasFound = false;
-    wss.clients.forEach((client) => {
-      if (client === ws) return false;
-      if (client.publicKey !== destinationPublicKey) return false;
-      if (client.readyState === WebSocket.OPEN) {
-        destionationWasFound = true;
-        client.send(data, { binary: isBinary });
+      if (textContent.startsWith("PING")) {
+        ws.send("PONG");
+        return;
       }
-    });
 
-    if (!destionationWasFound && retentionLevel === "1") {
-      console.log("MEESSAGE PUSHED TO QUEUE ", queue.length);
-      queue.push({
-        from: ws.publicKey,
-        to: destinationPublicKey,
-        data: data,
-        isBinary: isBinary,
+      const destinationPublicKey = textContent.split(" ")[1];
+      const retentionLevel = textContent.split(" ")[2];
+
+      // TODO: Mutex to ensure order or relayred messaged
+
+      let destionationWasFound = false;
+
+      wss.clients.forEach((client) => {
+        if (client === ws) return false;
+        if (client.publicKey !== destinationPublicKey) return false;
+        if (client.readyState === WebSocket.OPEN) {
+          destionationWasFound = true;
+          client.send(data, { binary: isBinary });
+        }
       });
-      if (queue.length >= MAX_QUEUE_SIZE) {
-        queue.shift();
+
+      if (!destionationWasFound && retentionLevel === "1") {
+        console.log("MEESSAGE PUSHED TO QUEUE ", queue.length);
+        queue.push({
+          from: ws.publicKey,
+          to: destinationPublicKey,
+          data: data,
+          isBinary: isBinary,
+        });
+        if (queue.length >= MAX_QUEUE_SIZE) {
+          queue.shift();
+        }
       }
+    } finally {
+      handlingMessage = false;
     }
   });
 });
